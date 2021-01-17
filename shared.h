9ba33cfff3f028761e0294dd7e0056d3afd5ec88
Craig Smoothey <craig@smoothey.org>
2021-01-17 16:20:49 +0000

    Added MacOS Support

--- shared.h
+++ shared.h
@@ -1,6 +1,12 @@
 #pragma once
+#if APL
+    #include <Carbon/Carbon.h>
+#endif
 
 void changeClockTime(float delta);
 void initDevice();
 void destroyWindow();
 void getPluginDir(char * buf);
+#if APL
+    int ConvertPath(const char * inPath, char * outPath, int outPathMaxLen);
+#endif
